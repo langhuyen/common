2dd1d0265968291967b482184852970d3bb9e9fe
Tran Thi Huyen 20166212 <huyen.tt166212@sis.hust.edu.vn>
2019-08-16 06:19:31 +0700

    check document

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+  </div>
+</template>
+
+<script>
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+// import config from "../config";
+export default {
+  name: "app",
+  data() {
+    return {
+      editor: ClassicEditor,
+      editorData: "<p>Content of the editor.</p>",
+      editorConfig: {}
+    };
+  }
+};
+</script>
